585cd300db9068dc4dd2b1bbbf87a1bb14a16723
elias-hezron <eliashezron23.com>
2021-01-10 17:05:58 +0300

    loader

--- frontend/src/components/Loader.js
+++ frontend/src/components/Loader.js
@@ -3,19 +3,8 @@ import {Spinner} from 'react-bootstrap'
 
 const Loader = () => {
     return (
-        <Spinner
-        //  animation = 'border' 
-        //  role = 'status'
-        //  style = {{
-        //      width: '100px',
-        //      height:'100px',
-        //      margin:'auto',
-        //      display:'block',
-        //  }}
-
-         >
-         <i class="fa fa-spinner fa-pulse fa-3x fa-fw fa-align-center" aria-hidden="true"></i>
-            <span className = 'sr-only'>Loading...</span>
+        <Spinner animation="grow">
+            <span className='sr-only'>Loading</span>
         </Spinner>
     )
 }
