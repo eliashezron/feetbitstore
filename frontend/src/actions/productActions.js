b56c6ea2c3e2d9b043adbcb34badbc61696295f0
eliashezron <eliashezron23@gmail.com>
2021-01-10 06:38:50 +0300

    Update productActions.js

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -30,7 +30,7 @@ export const listProducts = (keyword = '', pageNumber='' ) => async(dispatch)=>{
     try{
         dispatch({type: PRODUCT_LIST_REQUEST})
 
-        const {data} = await axios.get(`https://backend12345678910.herokuapp.com/api/products?keyword=${keyword}&pageNumber=${pageNumber}`)
+        const {data} = await axios.get(`/api/products?keyword=${keyword}&pageNumber=${pageNumber}`)
 
         dispatch({
             type: PRODUCT_LIST_SUCCESS,
