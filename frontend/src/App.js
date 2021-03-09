e3992859a1aefc21897a67cd289ec61b1690a40a
unknown <brianotweyo@gmail.com>
2021-03-09 14:40:03 +0300

    main-edit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,7 +22,7 @@ const App = () => {
   return ( 
   <Router>  
     <Header / >
-    <main  className='py-3'>
+    <main >
     <Container>  
     <Route path='/login' component={LoginScreen}/>
     <Route path='/destination' component={ShippingScreen}/>
