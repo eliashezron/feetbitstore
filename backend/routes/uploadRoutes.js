00575f0d22d4b956f5b15c8b9343f25498778c72
unknown <brianotweyo@gmail.com>
2021-03-18 16:15:02 +0300

    cloudinary-added

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,44 +1,50 @@
-import path from 'path'
-import express from 'express'
-import multer from 'multer'
+import path from 'path';
+import express from 'express';
+import dotenv from 'dotenv';
+import cloudinary from 'cloudinary';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import multer from 'multer';
+dotenv.config();
 
+const cloud = cloudinary.v2;
+const router = express.Router();
 
-const router = express.Router()
+cloud.config({
+cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+api_key: process.env.CLOUDINARY_API_KEY,
+api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
-const storage = multer.diskStorage({
-    destination(req, file, cb) {
-      cb(null, 'uploads/')
-    },
-    filename(req, file, cb) {
-      cb(
-        null,
-        `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
-      )
-    },
-  })
+const storage = new CloudinaryStorage({
+  cloudinary: cloud,
+  params: {
+      folder: 'folder_name', // any desirable name
+      public_id: (req, file) => `${file.originalname.split('.')[0]}-${Date.now()}`,
+  },
+});
 
-
-function checkFileType(file, cb){
-    const filetypes = /jpg|jpeg|png/
-    const extname = filetypes.test(path.extname(file.originalname).toLowerCase())
-    const mimetype = filetypes.test(file.mimetype)
-
-    if(extname && mimetype){
-        return cb(null, true)
-    }else{
-        cb('Images only!')
-    }
+function checkFileType(file, cb) {
+const filetypes = /jpg|jpeg|png/;
+const extname = filetypes.test(
+path.extname(file.originalname).toLocaleLowerCase()
+);
+const mimetype = filetypes.test(file.mimetype);
+if (extname && mimetype) {
+return cb(null, true);
+} else {
+cb(null, false);
+}
 }
 
 const upload = multer({
-    storage,
-    fileFilter:function(req, file, cb){
-        checkFileType(file, cb)
-    }
-})
+storage,
+fileFilter: function (req, file, cb) {
+checkFileType(file, cb);
+},
+});
 
-router.post('/', upload.single('image'), (req, res)=>{
-    res.send(`/${req.file.path}`)
-})
+router.post('/', upload.single('image'), (req, res) => {
+res.send(req.file.path);
+});
 
-export default router
+export default router;
