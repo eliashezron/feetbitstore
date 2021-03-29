6c71a345cb02bb229adbd7aafa0c61845a73222a
eliashezron <eliashezron23@gmail.com>
2021-03-29 08:31:32 +0000

    edit 106

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,46 +1,28 @@
-import express from 'express'
-const router = express.Router()
-import { uploader} from './config/cloudinaryConfig.js'
-import { multerUploads, dataUri} from './middleware/multer.js';
+import path from 'path';
+import express from 'express';
+import dotenv from 'dotenv';
+import cloudinary from 'cloudinary';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import multer from 'multer';
 
-router.post('/', multerUploads, (req, res) => {
-if(req.file) {
-const file = dataUri(req).content;
-return uploader.upload(file).then((result) => {
-const image = result.url;
-return res.status(200).json({
-messge: 'Your image has been uploded successfully to cloudinary',
-data: {
-image
-}
-})
-}).catch((err) => res.status(400).json({
-messge: 'someting went wrong while processing your request',
-data: {
-err
-}
-}))
-}
+dotenv.config();
+
+const cloud = cloudinary.v2;
+const router = express.Router();
+
+cloud.config({
+cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+api_key: process.env.CLOUDINARY_API_KEY,
+api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-export default router;
-// import path from 'path';
-// import express from 'express';
-// import dotenv from 'dotenv';
-// import cloudinary from 'cloudinary';
-// import { CloudinaryStorage } from 'multer-storage-cloudinary';
-// import multer from 'multer';
-// import { uploader} from './config/cloudinaryConfig.js'
-// import { multerUploads, dataUri} from './middleware/multer.js';
-// dotenv.config();
 
-// const cloud = cloudinary.v2;
-// const router = express.Router();
-
-// cloud.config({
-// cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-// api_key: process.env.CLOUDINARY_API_KEY,
-// api_secret: process.env.CLOUDINARY_API_SECRET,
-// });
+const storage = cloudinaryStorage({
+cloudinary: cloud,
+folder: "feetbitstores",
+allowedFormats: ["jpg", "png"],
+transformation: [{ width: 500, height: 500, crop: "limit" }],
+public_id: (req, file) => `${file.originalname.split('.')[0]}-${Date.now()}`
+});
 
 // const storage = new CloudinaryStorage({
 //   cloudinary: cloud,
@@ -50,28 +32,61 @@ export default router;
 //   },
 // });
 
-// function checkFileType(file, cb) {
-// const filetypes = /jpg|jpeg|png/;
-// const extname = filetypes.test(
-// path.extname(file.originalname).toLocaleLowerCase()
-// );
-// const mimetype = filetypes.test(file.mimetype);
-// if (extname && mimetype) {
-// return cb(null, true);
-// } else {
-// cb(null, false);
+function checkFileType(file, cb) {
+const filetypes = /jpg|jpeg|png/;
+const extname = filetypes.test(
+path.extname(file.originalname).toLocaleLowerCase()
+);
+const mimetype = filetypes.test(file.mimetype);
+if (extname && mimetype) {
+return cb(null, true);
+} else {
+cb(null, false);
+}
+}
+
+const upload = multer({
+storage,
+fileFilter: function (req, file, cb) {
+checkFileType(file, cb);
+},
+});
+
+router.post('/', upload.single('image'), (req, res) => {
+     console.log(req.file)
+const image = {};
+  image.url = req.file.url;
+  image.id = req.file.public_id;
+  Image.create(image) // save image information in database
+    .then(newImage => res.json(newImage))
+    .catch(err => console.log(err));
+});
+
+export default router;
+
+
+// import express from 'express'
+// const router = express.Router()
+// import { uploader} from './config/cloudinaryConfig.js'
+// import { multerUploads, dataUri} from './middleware/multer.js';
+
+// router.post('/', multerUploads, (req, res) => {
+// if(req.file) {
+// const file = dataUri(req).content;
+// return uploader.upload(file).then((result) => {
+// const image = result.url;
+// return res.status(200).json({
+// messge: 'Your image has been uploded successfully to cloudinary',
+// data: {
+// image
 // }
+// })
+// }).catch((err) => res.status(400).json({
+// messge: 'someting went wrong while processing your request',
+// data: {
+// err
 // }
-
-// const upload = multer({
-// storage,
-// fileFilter: function (req, file, cb) {
-// checkFileType(file, cb);
-// },
+// }))
+// }
 // });
-
-// router.post('/', upload.single('image'), (req, res) => {
-// res.send(req.file.path);
-// });
-
 // export default router;
