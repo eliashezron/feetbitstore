150e4a86a3ac27aab018f7c5e59c8473d45d62a1
eliashezron <eliashezron23@gmail.com>
2021-03-29 11:52:15 +0000

    edit 1021

--- frontend/src/components/ProductCarousel.js
+++ frontend/src/components/ProductCarousel.js
@@ -24,9 +24,7 @@ const ProductCarousel = () => {
                     <Link to={`/product/${product._id}`}>
                         <Image src={product.image} alt={product.name} fluid />
                         <Carousel.Caption className='carousel-caption'>
-                            <h2>
-                                {product.name}(UGX{product.price})
-                            </h2>
+                            
                         </Carousel.Caption>
                     </Link>
                 </Carousel.Item>
